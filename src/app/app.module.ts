f364a393f8edcb6c0f868742abba0c18790a38cc
andy alvia <andy.alvia.ing@outlook.com>
2024-01-17 21:38:46 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProductListComponent } from './product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http'; 
+import { MatIconModule } from '@angular/material/icon';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { ProductEditComponent } from './product-edit/product-edit.component'; // Agrega esta importación
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductCreateComponent,
+    ProductFormComponent,
+    ProductEditComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    BrowserModule, FormsModule,
+    ReactiveFormsModule,MatIconModule,
+    AppRoutingModule,HttpClientModule,PaginationModule.forRoot()
   ],
   providers: [
     provideClientHydration()
